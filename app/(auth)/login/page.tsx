216e735997676d3f4c955b49f8437722d728b562
JavaScript Space Projects <124654425+javascriptspace-projects@users.noreply.github.com>
2024-03-25 23:48:15 +0500

    Initial Commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from "@components/auth/login-form";
+
+export default function Login() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  );
+}
---
+++ components/auth/social.tsx
@@ -0,0 +1,5 @@
+export default function Social() {
+  return (
+    <div>Social</div>
+  )
+}
